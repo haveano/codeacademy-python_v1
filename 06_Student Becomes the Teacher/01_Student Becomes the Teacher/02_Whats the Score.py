b1ffff39f1ca7880e372b803583348a135c42198
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:53:52 +0100

    Create 02_Whats the Score.py

---
+++ 06_Student Becomes the Teacher/01_Student Becomes the Teacher/02_Whats the Score.py
@@ -0,0 +1,31 @@
+"""
+What's the Score?
+Great work!
+
+Instructions
+Now fill out your lloyd dictionary with the appropriate scores. To save you some time, we've filled out the rest for you.
+
+Homework: 90.0, 97.0, 75.0, 92.0
+Quizzes: 88.0, 40.0, 94.0
+Test Scores: 75.0, 90.0
+
+Make sure to include the decimal points so your grades are stored as floats! This will be important later.
+"""
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
