53a5bc3816dbd263c56ba02e960c679209f41eed
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:17:19 +0100

    Create 13_max().py

---
+++ 04_Functions/01_Functions/13_max().py
@@ -0,0 +1,15 @@
+"""
+max()
+The max() function takes any number of arguments and returns the largest one. ("Largest" can have odd definitions here, so it's best to use max() on integers and floats, where the results are straightforward, and not on other objects, like strings.)
+
+For example, max(1,2,3) will return 3 (the largest number in the set of arguments).
+
+Instructions
+Try out the max() function on line 3 of the editor. You can provide any number of integer or float arguments to max().
+"""
+
+# Set maximum to the max value of any set of numbers on line 3!
+
+maximum = max(1,2,3,4,5,6,7,8,9,11)
+
+print maximum
