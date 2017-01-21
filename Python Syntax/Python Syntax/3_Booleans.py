217b589b4a9517a6f73cfc5e200f8021bffe071b
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:29:20 +0100

    Create 3_Booleans.py

---
+++ Python Syntax/Python Syntax/3_Booleans.py
@@ -0,0 +1,12 @@
+# Set the variables to the values listed in the instructions!
+a = True
+b = False
+print a, b
+
+my_int = 7
+my_float=1.23
+my_bool=True
+
+print my_int
+print "\tasd"
+print my_float
