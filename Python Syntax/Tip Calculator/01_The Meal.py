5a9c8b46522906958c3b07c3b564a699c64ec02a
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:49:43 +0100

    Create 01_The Meal.py

---
+++ Python Syntax/Tip Calculator/01_The Meal.py
@@ -0,0 +1,17 @@
+"""
+The Meal
+Now let's apply the concepts from the previous section to a real world example.
+
+You've finished eating at a restaurant, and received this bill:
+
+Cost of meal: $44.50
+Restaurant tax: 6.75%
+Tip: 15%
+You'll apply the tip to the overall cost of the meal (including tax).
+
+Instructions
+First, let's declare the variable meal and assign it the value 44.50.
+"""
+# Assign the variable meal the value 44.50 on line 3!
+
+meal = 44.50
