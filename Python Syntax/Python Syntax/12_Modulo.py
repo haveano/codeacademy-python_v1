2f935649a0aeddc96b408cba3bbed640aac50cee
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:47:40 +0100

    Update 12_Modulo.py

--- Python Syntax/Python Syntax/12_Modulo.py
+++ Python Syntax/Python Syntax/12_Modulo.py
@@ -1,3 +1,10 @@
+"""
+Modulo
+Our final operator is modulo. Modulo returns the remainder from a division. So, if you type 3 % 2, it will return 1, because 2 goes into 3 evenly once, with 1 left over.
+
+Instructions
+Use modulo to set spam equal to 1. You can use any two numbers that will leave a remainder of 1 to do this.
+"""
 #Set spam equal to 1 using modulo on line 3!
 
 spam = 13%4
