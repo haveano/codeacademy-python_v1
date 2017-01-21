44400978b63bd01e5c09bb81a6f7b7f55ce82377
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:51:14 +0100

    Create 03_The Tip.py

---
+++ Python Syntax/Tip Calculator/03_The Tip.py
@@ -0,0 +1,14 @@
+"""
+The Tip
+Nice work! You received good service, so you'd like to leave a 15% tip on top of the cost of the meal, including tax.
+
+Before we compute the tip for your bill, let's set a variable for the tip. Again, we need to get the decimal form of the tip, so we divide 15.0 by 100.
+
+Instructions
+Set the variable tip to decimal value of 15% on line 5.
+"""
+# You're almost there! Assign the tip variable on line 5.
+
+meal = 44.50
+tax = 0.0675
+tip = 0.15
