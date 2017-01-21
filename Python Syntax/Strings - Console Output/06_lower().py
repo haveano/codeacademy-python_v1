d390c2c58bdca8a59ed0ae68dd30e7b6ee632e85
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 22:08:10 +0100

    Create 06_lower().py

---
+++ Python Syntax/Strings - Console Output/06_lower().py
@@ -0,0 +1,15 @@
+"""
+lower()
+Well done!
+
+You can use the lower() method to get rid of all the capitalization in your strings. You call lower() like so:
+
+"Ryan".lower()
+which will return "ryan".
+
+Instructions
+Call lower() on parrot (after print) on line 3 in the editor.
+"""
+parrot = "Norwegian Blue"
+
+print parrot.lower()
