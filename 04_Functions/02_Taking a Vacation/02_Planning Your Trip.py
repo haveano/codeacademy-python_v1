b572a0030dd710c821a49a75df62df480e48bc6a
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:24:21 +0100

    Create 02_Planning Your Trip.py

---
+++ 04_Functions/02_Taking a Vacation/02_Planning Your Trip.py
@@ -0,0 +1,17 @@
+"""
+Planning Your Trip
+When planning a vacation, it's very important to know exactly how much you're going to spend.
+
+def wages(hours):
+    # If I make $8.35/hour...
+    return 8.35 * hours
+The above example is just a refresher in how functions are defined.
+
+Let's use functions to calculate your trip's costs.
+
+Instructions
+Define a function called hotel_cost with one argument nights as input.
+The hotel costs $140 per night. So, the function hotel_cost should return 140 * nights.
+"""
+def hotel_cost(nights):
+    return 140*nights
