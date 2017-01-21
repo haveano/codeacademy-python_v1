1bed1d64a71067591d06c91a0b2acf10d89910b6
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:39:49 +0100

    Create 13_Remove a Few Things.py

---
+++ 05_Lists and Dictionaries/01_Python Lists and Dictionaries/13_Remove a Few Things.py
@@ -0,0 +1,20 @@
+"""
+Remove a Few Things
+Sometimes you need to remove something from a list.
+
+beatles = ["john","paul","george","ringo","stuart"]
+beatles.remove("stuart")
+print beatles
+>> ["john","paul","george","ringo"]
+We create a list called beatles with 5 strings.
+Then, we remove the first item from beatles that matches the string "stuart". Note that .remove(item) does not return anything.
+Finally, we print out that list just to see that "stuart" was actually removed.
+Instructions
+Remove 'dagger' from the list of items stored in the backpack variable.
+"""
+
+
+backpack = ['xylophone', 'dagger', 'tent', 'bread loaf']
+print backpack
+backpack.remove("dagger")
+print backpack
