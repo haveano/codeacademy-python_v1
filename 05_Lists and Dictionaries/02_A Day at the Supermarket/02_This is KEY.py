34a9ed1bdca2ecf4579decd218e65693e018332f
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:43:14 +0100

    Create 02_This is KEY.py

---
+++ 05_Lists and Dictionaries/02_A Day at the Supermarket/02_This is KEY.py
@@ -0,0 +1,25 @@
+"""
+This is KEY!
+You can also use a for loop on a dictionary to loop through its keys with the following:
+
+# A simple dictionary
+d = {"foo" : "bar"}
+
+for key in d: 
+    print d[key]  # prints "bar" 
+Note that dictionaries are unordered, meaning that any time you loop through a dictionary, you will go through every key, but you are not guaranteed to get them in any particular order.
+
+Instructions
+Use a for loop to go through the webster dictionary and print out all of the definitions.
+"""
+
+webster = {
+	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+# Add your code below!
+for x in webster:
+    print webster[x]
