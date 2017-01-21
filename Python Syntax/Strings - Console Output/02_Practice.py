27dc8b396ed8ad438fccfb2ef799c596b629e197
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:55:15 +0100

    Create 02_Practice.py

---
+++ Python Syntax/Strings - Console Output/02_Practice.py
@@ -0,0 +1,24 @@
+"""
+Practice
+Excellent! Let's get a little practice in with strings.
+
+Instructions
+Set the following variables to their respective phrases:
+
+Set caesar to "Graham"
+Set praline to "John"
+Set viking to "Teresa"
+"""
+# Assign your variables below, each on its own line!
+
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+
+
+# Put your variables above this line
+
+print caesar
+print praline
+print viking
