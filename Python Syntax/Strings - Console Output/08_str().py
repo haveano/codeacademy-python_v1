a8388281c13ac64dc2573c6da986e8db355a61cd
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 22:09:24 +0100

    Create 08_str().py

---
+++ Python Syntax/Strings - Console Output/08_str().py
@@ -0,0 +1,16 @@
+"""
+str()
+Now let's look at str(), which is a little less straightforward. The str() method turns non-strings into strings! For example:
+
+str(2)
+would turn 2 into "2".
+
+Instructions
+Create a variable pi and set it to 3.14 on line 4.
+Call str(pi) on line 5, after print.
+"""
+"""Declare and assign your variable on line 4,
+then call your method on line 5!"""
+pi=3.14
+
+print str(pi)
