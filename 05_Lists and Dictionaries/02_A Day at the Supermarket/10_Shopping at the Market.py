d15a56aa13b77b083516484a50ca82d3967c0d8b
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:49:23 +0100

    Create 10_Shopping at the Market.py

---
+++ 05_Lists and Dictionaries/02_A Day at the Supermarket/10_Shopping at the Market.py
@@ -0,0 +1,12 @@
+"""
+Shopping at the Market
+Great work! Now we're going to take a step back from the management side and take a look through the eyes of the shopper.
+
+In order for customers to order online, we are going to have to make a consumer interface. Don't worry: it's easier than it sounds!
+
+Instructions
+First, make a list called groceries with the values "banana","orange", and "apple".
+"""
+
+groceries=["banana","orange","apple"]
+
