582914157724d6842ab79c61717927c10a0ddf04
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:21:00 +0100

    Create 18_Review Modules.py

---
+++ 04_Functions/01_Functions/18_Review Modules.py
@@ -0,0 +1,12 @@
+"""
+Review: Modules
+Good work! Now let's see what you remember about importing modules (and, specifically, what's available in the math module).
+
+Instructions
+Import the math module in whatever way you prefer. Call its sqrt function on the number 13689 and print that value to the console.
+"""
+
+from math import sqrt
+#import math from sqrt 
+
+print sqrt(13689)
