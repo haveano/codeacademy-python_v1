b81edcb66b2c59228ade6ea9734792cc82c6db92
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:42:12 +0100

    Update and rename 4_You've Been Reassigned.py to 04_You've Been Reassigned.py

--- Python Syntax/Python Syntax/04_You've Been Reassigned.py
+++ Python Syntax/Python Syntax/04_You've Been Reassigned.py
@@ -1,3 +1,13 @@
+"""
+You've Been Reassigned
+Now you know how to use variables to store values.
+
+Say my_int = 7. You can change the value of a variable by "reassigning" it, like this:
+
+my_int = 3
+Instructions
+Try it and see! Change the value of my_int from 7 to 3 in the editor
+"""
 # my_int is set to 7 below. What do you think
 # will happen if we reset it to 3 and print the result?
 
