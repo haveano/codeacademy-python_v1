dcdccee1787ee9d19e465d43481cb6502039bb50
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 23:07:33 +0100

    Create 02_Compare Closely.py

---
+++ 03_Conditionals and Control Flow/01_Conditionals and Control Flow/02_Compare Closely.py
@@ -0,0 +1,39 @@
+"""
+Compare Closely!
+Let's start with the simplest aspect of control flow: comparators. There are six:
+
+Equal to (==)
+Not equal to (!=)
+Less than (<)
+Less than or equal to (<=)
+Greater than (>)
+Greater than or equal to (>=)
+Comparators check if a value is (or is not) equal to, greater than (or equal to), or less than (or equal to) another value.
+
+Note that == compares whether two things are equal, and = assigns a value to a variable.
+
+Instructions
+Set each variable to True or False depending on what you think the result will be. For example, 1 < 2 will be True, because one is less than two.
+
+Set bool_one equal to the result of 17 < 328
+Set bool_two equal to the result of 100 == (2 * 50)
+Set bool_three equal to the result of 19 <= 19
+Set bool_four equal to the result of -22 >= -18
+Set bool_five equal to the result of 99 != (98 + 1)
+"""
+# Assign True or False as appropriate on the lines below!
+
+# Set this to True if 17 < 328 or to False if it is not.
+bool_one = True   # We did this one for you!
+
+# Set this to True if 100 == (2 * 50) or to False otherwise.
+bool_two = True
+
+# Set this to True if 19 <= 19 or to False if it is not.
+bool_three = True
+
+# Set this to True if -22 >= -18 or to False if it is not.
+bool_four = False
+
+# Set this to True if 99 != (98 + 1) or to False otherwise.
+bool_five = False
