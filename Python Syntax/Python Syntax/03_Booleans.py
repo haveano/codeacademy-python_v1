82ff14b7c80a607f522915748aaace9b74429f3b
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:41:53 +0100

    Update and rename 3_Booleans.py to 03_Booleans.py

---
+++ Python Syntax/Python Syntax/03_Booleans.py
@@ -0,0 +1,29 @@
+"""
+Booleans
+Great! You just stored a number in a variable. Numbers are one data type we use in programming. A second data type is called a boolean.
+
+A boolean is like a light switch. It can only have two values. Just like a light switch can only be on or off, a boolean can only be True or False.
+
+You can use variables to store booleans like this:
+
+a = True
+b = False
+Instructions
+Set the following variables to the corresponding values:
+
+my_int to the value 7
+my_float to the value 1.23
+my_bool to the value True
+"""
+# Set the variables to the values listed in the instructions!
+a = True
+b = False
+print a, b
+
+my_int = 7
+my_float=1.23
+my_bool=True
+
+print my_int
+print "\tasd"
+print my_float
