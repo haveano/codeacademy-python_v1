a89dc656465ff02c30778f27fc798d293cf5cd10
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:23:09 +0100

    Create 01_Before We Begin.py

---
+++ 04_Functions/02_Taking a Vacation/01_Before We Begin.py
@@ -0,0 +1,28 @@
+"""
+Before We Begin
+Let's first quickly review functions in Python.
+
+def bigger(first, second):
+    print max(first, second)
+    return True
+In the example above:
+
+We define a function called bigger that has two arguments called first and second.
+Then, we print out the larger of the two arguments using the built-in function max.
+Finally, the bigger function returns True.
+Now try creating a function yourself!
+
+Instructions
+Write a function called answer that takes no arguments and returns the value 42.
+
+Even without arguments, you will still need parentheses.
+
+Don't forget the colon at the end of the function definition!
+
+"""
+
+def answer():
+    return 42
+
+answer()
+#print answer()
