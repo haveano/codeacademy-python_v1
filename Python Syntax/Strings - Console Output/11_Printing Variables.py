8fcbb29e4be9c263d64d3ddd326a1c9c43473a6a
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 22:11:36 +0100

    Create 11_Printing Variables.py

---
+++ Python Syntax/Strings - Console Output/11_Printing Variables.py
@@ -0,0 +1,14 @@
+"""
+Printing Variables
+Great! Now that we've printed strings, let's print variables
+
+Instructions
+Declare a variable called the_machine_goes and assign it the string value "Ping!" on line 5.
+Go ahead and print the_machine_goes in line 6.
+"""
+"""Assign the string "Ping!" to
+the variable the_machine_goes on
+line 5, then print it out on line 6!"""
+
+the_machine_goes ="Ping!"
+print the_machine_goes
