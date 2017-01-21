d9bac12162f6352b006f4707a85211e74a2af7b9
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:50:23 +0100

    Create 02_The Tax.py

---
+++ Python Syntax/Tip Calculator/02_The Tax.py
@@ -0,0 +1,11 @@
+"""
+The Tax
+Good! Now let's create a variable for the tax percentage.
+
+The tax on your receipt is 6.75%. You'll have to divide 6.75 by 100 in order to get the decimal form of the percentage. (See the Hint if you would like further explanation.)
+
+Instructions
+Create the variable tax and set it equal to the decimal value of 6.75%.
+"""
+meal = 44.50
+tax = 6.75*0.01
