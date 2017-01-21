129fe34ca609ccadd6433635d9a2ecebd6ff03ad
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:48:09 +0100

    Update 13_Bringing It All Together.py

--- Python Syntax/Python Syntax/13_Bringing It All Together.py
+++ Python Syntax/Python Syntax/13_Bringing It All Together.py
@@ -1,3 +1,20 @@
+"""
+Bringing It All Together
+Nice work! So far, you've learned about:
+
+Variables, which store values for later use
+Data types, such as numbers and booleans
+Whitespace, which separates statements
+Comments, which make your code easier to read
+Arithmetic operations, including +, -, *, /, **, and %
+Instructions
+Let's put our knowledge to work.
+
+Write a single-line comment on line 1. It can be anything! (Make sure it starts with #)
+Set the variable monty equal to True.
+Set another variable python equal to 1.234.
+Set a third variable monty_python equal to python squared.
+"""
 # to jest komentarz
 monty = True
 python=1.234
