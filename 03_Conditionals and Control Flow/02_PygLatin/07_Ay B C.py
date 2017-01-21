a3da3a44d072c6c4c132a98e18cbafd1ea7a094d
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 23:23:20 +0100

    Create 07_Ay B C.py

---
+++ 03_Conditionals and Control Flow/02_PygLatin/07_Ay B C.py
@@ -0,0 +1,14 @@
+"""
+Ay B C
+Now we can get ready to start translating to Pig Latin! Let's review the rules for translation:
+
+You move the first letter of the word to the end and then append the suffix 'ay'.
+Example: python -> ythonpay
+
+Let's create a variable to hold our translation suffix.
+
+Instructions
+Create a variable named pyg and set it equal to the suffix 'ay'.
+"""
+
+pyg="ay"
