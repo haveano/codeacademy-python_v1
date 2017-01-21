24ee1d3eb3ee685466cda468cfa649831912f7f6
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:18:47 +0100

    Create 15_abs().py

---
+++ 04_Functions/01_Functions/15_abs().py
@@ -0,0 +1,12 @@
+"""
+abs()
+The abs() function returns the absolute value of the number it takes as an argument—that is, that number's distance from 0 on an imagined number line. For instance, 3 and -3 both have the same absolute value: 3. The abs() function always returns a positive value, and unlike max() and min(), it only takes a single number.
+
+Instructions
+Set absolute equal to the absolute value of -42 on line 2.
+"""
+
+
+absolute = abs(-42)
+
+print absolute
