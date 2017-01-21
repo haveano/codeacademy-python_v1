8ce73b5db2b392b5bc2bb488e0f2f29d80e7c447
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:45:55 +0100

    Update 10_Math.py

--- Python Syntax/Python Syntax/10_Math.py
+++ Python Syntax/Python Syntax/10_Math.py
@@ -1,3 +1,14 @@
+"""
+Math
+Great! Now let's do some math. You can add, subtract, multiply, divide numbers like this
+
+addition = 72 + 23
+subtraction = 108 - 204
+multiplication = 108 * 0.5
+division = 108 / 9
+Instructions
+Set the variable count_to equal to the sum of two big numbers.
+"""
 # Set count_to equal to the sum of two big numbers
 count_to = 123+124
 
