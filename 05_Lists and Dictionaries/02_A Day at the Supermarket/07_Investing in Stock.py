04343313c554c6c87af3cb3a5542992e1ccd060b
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:47:34 +0100

    Create 07_Investing in Stock.py

---
+++ 05_Lists and Dictionaries/02_A Day at the Supermarket/07_Investing in Stock.py
@@ -0,0 +1,33 @@
+"""
+Investing in Stock
+Good work! As a store manager, you’re also in charge of keeping track of your stock/inventory.
+
+Instructions
+Create a stock dictionary with the values below.
+
+"banana": 6
+"apple": 0
+"orange": 32
+"pear": 15
+"""
+
+
+prices={
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+stock={
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+for y in stock:
+    print stock[y]
+
+for y in prices:
+    print prices[y]
