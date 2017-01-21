feb40c3f040068267879c09bd7d3ff60c0bb18db
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:54:28 +0100

    Create 03_Put It Together.py

---
+++ 06_Student Becomes the Teacher/01_Student Becomes the Teacher/03_Put It Together.py
@@ -0,0 +1,31 @@
+"""
+Put It Together
+Now lets put the three dictionaries in a list together.
+
+my_list = [1, 2, 3]
+The above example is just a reminder on how to create a list. Afterwards, my_list contains 1, 2, and 3.
+
+Instructions
+Below your code, create a list called students that contains lloyd, alice, and tyler.
+"""
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+students=[lloyd,alice,tyler]
+print students
