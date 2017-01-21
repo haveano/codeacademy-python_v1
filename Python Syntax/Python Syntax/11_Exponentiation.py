8524275f9e1b17578e54358253bd5668577804f2
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:47:17 +0100

    Update 11_Exponentiation.py

--- Python Syntax/Python Syntax/11_Exponentiation.py
+++ Python Syntax/Python Syntax/11_Exponentiation.py
@@ -1,3 +1,19 @@
+"""
+Exponentiation
+All that math can be done on a calculator, so why use Python? Because you can combine math with other data types (e.g. booleans) and commands to create useful programs. Calculators just stick to numbers.
+
+Now let's work with exponents.
+
+eight = 2 ** 3
+In the above example, we create a new variable called eight and set it to 8, or the result of 2 to the power to 3 (2^3).
+
+Notice that we use ** instead of * or the multiplication operator.
+
+Instructions
+Create a new variable called eggs and use exponents to set eggs equal 100.
+
+Try raising 10 to the power of 2.
+"""
 #Set eggs equal to 100 using exponentiation on line 3!
 
 eggs = 10**2
