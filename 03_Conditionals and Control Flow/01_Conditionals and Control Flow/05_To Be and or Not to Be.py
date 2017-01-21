7c373551ff42f1b1ef8e60b10aa8c15b69f48116
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 23:10:01 +0100

    Create 05_To Be and or Not to Be.py

---
+++ 03_Conditionals and Control Flow/01_Conditionals and Control Flow/05_To Be and or Not to Be.py
@@ -0,0 +1,31 @@
+"""
+To Be and/or Not to Be
+Boolean operators compare statements and result in boolean values. There are three boolean operators:
+
+and, which checks if both the statements are True;
+or, which checks if at least one of the statements is True;
+not, which gives the opposite of the statement.
+We'll go through the operators one by one.
+
+Instructions
+Look at the truth table in the editor. Don't worry if you don't completely get it yet—you will by the end of this section!
+
+Click Save & Submit to continue.
+"""
+"""
+     Boolean Operators
+------------------------      
+True and True is True
+True and False is False
+False and True is False
+False and False is False
+
+True or True is True
+True or False is True
+False or True is True
+False or False is False
+
+Not True is False
+Not False is True
+
+"""
