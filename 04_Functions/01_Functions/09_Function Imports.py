f54e8a3a012c7ae6430757ba63cf7ad69ad95121
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:08:23 +0100

    Create 09_Function Imports.py

---
+++ 04_Functions/01_Functions/09_Function Imports.py
@@ -0,0 +1,17 @@
+"""
+Function Imports
+Nice work! Now Python knows how to take the square root of a number.
+
+However, we only really needed the sqrt function, and it can be frustrating to have to keep typing math.sqrt().
+
+It's possible to import only certain variables or functions from a given module. Pulling in just a single function from a module is called a function import, and it's done with the from keyword:
+
+from module import function
+Now you can just type sqrt() to get the square root of a number—no more math.sqrt()!
+
+Instructions
+Let's import only the sqrt function from math this time. (You don't need the () after sqrt in the from math import sqrt bit.)
+"""
+# Import *just* the sqrt function from math on line 3!
+from math import sqrt
+
