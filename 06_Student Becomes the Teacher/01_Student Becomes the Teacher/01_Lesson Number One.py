93b46c372b7b9c4c5285bb41f8d259b8ed061583
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:53:10 +0100

    Create 01_Lesson Number One.py

---
+++ 06_Student Becomes the Teacher/01_Student Becomes the Teacher/01_Lesson Number One.py
@@ -0,0 +1,39 @@
+"""
+Lesson Number One
+Welcome to this "Challenge Course". Until now we've been leading you by the hand and working on some short and relatively easy projects. This is a challenge so be ready. We have faith in you!
+
+We’re going to switch it up a bit and allow you to be the teacher of your own class. Make a gradebook for all of your students.
+
+animal_sounds = {
+    "cat": ["meow", "purr"],
+    "dog": ["woof", "bark"],
+    "fox": [],
+}
+print animal_sounds["cat"]
+The example above is just to remind you how to create a dictionary and then to access the item stored by the "cat" key.
+
+Instructions
+Create three dictionaries: lloyd, alice, and tyler.
+Give each dictionary the keys "name", "homework", "quizzes", and "tests".
+Have the "name" key be the name of the student (that is, lloyd's name should be "Lloyd") and the other keys should be an empty list. (We'll fill in these lists soon!)
+"""
+
+
+lloyd={
+    "name":"Lloyd",
+    "homework":[],
+    "quizzes":[],
+    "tests":[],
+}
+alice={
+    "name":"Alice",
+    "homework":[],
+    "quizzes":[],
+    "tests":[],
+}
+tyler={
+    "name":"Tyler",
+    "homework":[],
+    "quizzes":[],
+    "tests":[],
+}
