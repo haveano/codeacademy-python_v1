8bdfea910f6594c83c3beb7c8a41481a89db38a8
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 22:16:11 +0100

    Create 01_The datetime Library.py

---
+++ Python Syntax/04 Date and Time/01_The datetime Library.py
@@ -0,0 +1,11 @@
+"""
+The datetime Library
+A lot of times you want to keep track of when something happened. We can do so in Python using datetime.
+
+Here we'll use datetime to print the date and time in a nice format.
+
+Instructions
+Click Save & Submit Code to continue.
+"""
+from datetime import datetime
+
