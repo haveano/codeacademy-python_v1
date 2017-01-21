d2f05c0b0f7f502867e2e63f119d1d87b8150c05
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:47:00 +0100

    Create 06_Your Own Store.py

---
+++ 05_Lists and Dictionaries/02_A Day at the Supermarket/06_Your Own Store.py
@@ -0,0 +1,30 @@
+"""
+Your Own Store!
+Okay—on to the core of our project.
+
+Congratulations! You are now the proud owner of your very own Codecademy brand supermarket.
+
+animal_counts = {
+    "ant": 3,
+    "bear": 6,
+    "crow": 2
+}
+In the example above, we create a new dictionary called animal_counts with three entries. One of the entries has the key "ant" and the value 3.
+
+Instructions
+Create a new dictionary called prices using {} format like the example above.
+Put these values in your prices dictionary, in between the {}:
+"banana": 4,
+"apple": 2,
+"orange": 1.5,
+"pear": 3
+Yeah, this place is really expensive. (Your supermarket subsidizes the zoo from the last course.)
+"""
+
+
+prices={
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
