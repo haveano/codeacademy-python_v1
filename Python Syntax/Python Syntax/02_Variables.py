bc4a25fffb95c0b178d5286a823351c0e87dc8f8
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:40:29 +0100

    Update and rename 2_Variables.py to 02_Variables.py

---
+++ Python Syntax/Python Syntax/02_Variables.py
@@ -0,0 +1,18 @@
+"""
+Variables
+Creating web apps, games, and search engines all involve storing and working with different types of data. They do so using variables. A variable stores a piece of data, and gives it a specific name.
+
+For example:
+
+spam = 5
+The variable spam now stores the number 5.
+
+Instructions
+Set the variable my_variable equal to the value 10.
+Click the Save & Submit button to run your code.
+"""
+# to jest komentarz
+# Write your code below!
+spam = 5
+print spam
+my_variable = 10
--- Python Syntax/Python Syntax/2_Variables.py
+++
@@ -1,5 +0,0 @@
-# to jest komentarz
-# Write your code below!
-spam = 5
-print spam
-my_variable = 10
