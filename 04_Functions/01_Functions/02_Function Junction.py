e121d932df55398de4813791ff1cbd6ddd58b7db
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:01:23 +0100

    Create 02_Function Junction.py

---
+++ 04_Functions/01_Functions/02_Function Junction.py
@@ -0,0 +1,33 @@
+"""
+Function Junction
+Functions are defined with three components:
+
+The header, which includes the def keyword, the name of the function, and any parameters the function requires. Here's an example:
+
+def hello_world(): // There are no parameters
+An optional comment that explains what the function does.
+
+"""Prints 'Hello World!' to the console."""
+The body, which describes the procedures the function carries out. The body is indented, just like for conditional statements.
+
+print "Hello World!"
+Here's the full function pieced together:
+
+def hello_world():
+    """Prints 'Hello World!' to the console."""
+    print "Hello World!"
+Instructions
+Go ahead and create a function, spam, that prints the string "Eggs!" to the console. Don't forget to include a comment of your own choosing (enclose it in triple quotes!).
+"""
+# Define your spam function starting on line 5. You
+# can leave the code on line 11 alone for now--we'll
+# explain it soon!
+def spam():
+    """To jest jakiś komentarz"""
+    print "Eggs!"
+    return 0
+
+
+
+# Define the spam function above this line.
+spam()
