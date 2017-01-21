3c1bf8ef7a3b96a7044702e2b072980a02d9d8ac
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 23:17:36 +0100

    Create 01_Break It Down.py

---
+++ 03_Conditionals and Control Flow/02_PygLatin/01_Break It Down.py
@@ -0,0 +1,16 @@
+"""
+Break It Down
+Now let's take what we've learned so far and write a Pig Latin translator.
+
+Pig Latin is a language game, where you move the first letter of the word to the end and add "ay." So "Python" becomes "ythonpay." To write a Pig Latin translator in Python, here are the steps we'll need to take:
+
+Ask the user to input a word in English.
+Make sure the user entered a valid word.
+Convert the word from English to Pig Latin.
+Display the translation result.
+Instructions
+When you're ready to get coding, click Save and Submit. Since we took the time to write out the steps for our solution, you'll know what's coming next!
+"""
+
+raw_input("Wpisz text po andgielsku: ")
+
