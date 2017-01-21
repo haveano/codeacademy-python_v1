6f56c858246cb725f0f18de24be53e8fe574bb38
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:38:28 +0100

    Update and rename 1_Welcome.py to 01_Welcome.py

---
+++ Python Syntax/Python Syntax/01_Welcome.py
@@ -0,0 +1,18 @@
+"""
+Welcome!
+Python is an easy to learn programming language. You can use it to create web apps, games, even a search engine!
+
+Ready to learn Python? Click Save & Submit Code to continue!
+
+Instructions
+Ready to learn Python? Click Save & Submit Code to continue!
+
+?
+Hint
+Get live, fast support from Codecademy Advisors when you get stuck.
+Learn more.
+If the loading bar fills but doesn't fade away, try refreshing the page.
+"""
+
+print "Welcome to Python! :)) \n \t sadf \n \n asfd"
+print "Welcome to Python!"
--- Python Syntax/Python Syntax/1_Welcome.py
+++
@@ -1,2 +0,0 @@
-print "Welcome to Python! :)) \n \t sadf \n \n asfd"
-print "Welcome to Python!"
