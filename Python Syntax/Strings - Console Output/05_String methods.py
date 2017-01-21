1584ed4a25e9c3374fdd31b576e341e0ded947c6
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 22:07:31 +0100

    Create 05_String methods.py

---
+++ Python Syntax/Strings - Console Output/05_String methods.py
@@ -0,0 +1,19 @@
+"""
+String methods
+Great work! Now that we know how to store strings, let's see how we can change them using string methods.
+
+String methods let you perform specific tasks for strings.
+
+We'll focus on four string methods:
+
+len()
+lower()
+upper()
+str()
+Let's start with len(), which gets the length (the number of characters) of a string!
+
+Instructions
+On line 1, create a variable named parrot and set it to the string "Norwegian Blue". On line 2, type len(parrot) after the word print, like so: print len(parrot). The output will be the number of letters in "Norwegian Blue"!
+"""
+parrot = "Norwegian Blue"
+print len(parrot)
