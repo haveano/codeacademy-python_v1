9078bd5c0477ceede1d541da3f60e6cbf3b1e1c4
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:05:39 +0100

    Create 07_I Know Kung Fu.py

---
+++ 04_Functions/01_Functions/07_I Know Kung Fu.py
@@ -0,0 +1,14 @@
+"""
+I Know Kung Fu
+Remember import this from the first exercise in this course? That was an example of importing a module. A module is a file that contains definitions—including variables and functions—that you can use once it is imported.
+
+Instructions
+Before we try any fancy importing, let's see what Python already knows about square roots. On line 3 in the editor, ask Python to
+
+print sqrt(25)
+which we would expect to equal five.
+"""
+
+# Ask Python to print sqrt(25) on line 3.
+
+print sqrt(25)
