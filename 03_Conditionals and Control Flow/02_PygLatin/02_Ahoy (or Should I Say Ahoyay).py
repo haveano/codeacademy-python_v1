006ffdbf12977a61b19e8bd72c3f88e6017f3bba
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 23:18:28 +0100

    Create 02_Ahoy (or Should I Say Ahoyay).py

---
+++ 03_Conditionals and Control Flow/02_PygLatin/02_Ahoy (or Should I Say Ahoyay).py
@@ -0,0 +1,10 @@
+"""
+Ahoy! (or Should I Say Ahoyay!)
+Let's warm up by printing a welcome message for our translator users.
+
+Instructions
+Please print the phrase "Pig Latin".
+"""
+print "Pig Latin"
+a = raw_input("Wpisz text po andgielsku: ")
+print a
