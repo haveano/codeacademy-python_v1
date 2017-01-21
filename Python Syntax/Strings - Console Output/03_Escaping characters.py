bb226b9d08720843d9b6c8e235d1ab7634e7fe9c
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:55:46 +0100

    Create 03_Escaping characters.py

---
+++ Python Syntax/Strings - Console Output/03_Escaping characters.py
@@ -0,0 +1,14 @@
+"""
+Escaping characters
+There are some characters that cause problems. For example:
+
+'There's a snake in my boot!'
+This code breaks because Python thinks the apostrophe in 'There's' ends the string. We can use the backslash to fix the problem, like this:
+
+'There\'s a snake in my boot!'
+Instructions
+Fix the string in the editor!
+"""
+# The string below is broken. Fix it using the escape backslash!
+
+'This isn\'t flying, this is falling with style!'
