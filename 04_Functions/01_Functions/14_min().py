38dc214f5b6507a854d751f40a95c6427a80a726
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:18:06 +0100

    Create 14_min().py

---
+++ 04_Functions/01_Functions/14_min().py
@@ -0,0 +1,13 @@
+"""
+min()
+min() then returns the smallest of a given series of arguments.
+
+Instructions
+Go ahead and set minimum equal to the min() of any set of integers or floats you'd like.
+"""
+
+# Set minimum to the min value of any set of numbers on line 3!
+
+minimum = min(1,2,3,4,56,7,8)
+
+print minimum
