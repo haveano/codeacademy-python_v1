9b2397c8a7ca5bbf0ff3acc3f79230cb45540302
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:12:04 +0100

    Create 10_Universal Imports.py

---
+++ 04_Functions/01_Functions/10_Universal Imports.py
@@ -0,0 +1,15 @@
+"""
+Universal Imports
+Great! We've found a way to handpick the variables and functions we want from modules.
+
+What if we still want all of the variables and functions in a module but don't want to have to constantly type math.?
+
+Universal import can handle this for you. The syntax for this is:
+
+from module import *
+Instructions
+Use the power of from module import * to import everything from the math module on line 3 of the editor.
+"""
+# Import *everything* from the math module on line 3!
+
+from math import *
