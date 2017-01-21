57eb1f1c0aab10307d5dc2229e0b70a554bf7e96
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 22:10:53 +0100

    Create 10_Printing Strings.py

---
+++ Python Syntax/Strings - Console Output/10_Printing Strings.py
@@ -0,0 +1,15 @@
+"""
+Printing Strings
+The area where we've been writing our code is called the editor.
+
+The console (the window in the upper right) is where the results of your code is shown.
+
+print simply displays your code in the console.
+
+Instructions
+Print "Monty Python" to the console.
+"""
+"""Tell Python to print "Monty Python"
+to the console on line 4!"""
+
+print "Monty Python"
