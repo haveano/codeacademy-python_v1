1ea4a69181f4a7a9cae89f84e94cea50199922a0
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:31:44 +0100

    Create 5_Whitespace.py

---
+++ Python Syntax/Python Syntax/5_Whitespace.py
@@ -0,0 +1,14 @@
+def spam():
+    eggs = 12
+    return eggs
+
+def dupa():
+    kupy = 12+3     #15
+    siki = 24-kupy  #9
+    #print kupy      #15
+    #print siki      #9
+    return kupy+siki    #24
+#    return siki
+
+print spam()
+print dupa()
