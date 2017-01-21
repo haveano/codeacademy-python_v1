1f4e9f7cdf7e8e28f61fc1c56d4acfc9554f4b84
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 21:43:02 +0100

    Update 05_Whitespace.py

--- Python Syntax/Python Syntax/05_Whitespace.py
+++ Python Syntax/Python Syntax/05_Whitespace.py
@@ -1,3 +1,12 @@
+"""
+Whitespace
+In Python, whitespace is used to structure code. Whitespace is important, so you have to be careful with how you use it.
+
+Instructions
+The code on the right is badly formatted. Hit "Save & Submit Code" to see what happens.
+
+You should see an error message. We'll fix it in the next exercise!
+"""
 def spam():
     eggs = 12
     return eggs
