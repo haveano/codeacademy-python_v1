87947448ba54cd34d0e093b1267d39482836eb45
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 23:01:31 +0100

    Create 02_Getting the Current Date and Time.py

---
+++ 02_Strings and Console Output/02_Date and Time/02_Getting the Current Date and Time.py
@@ -0,0 +1,19 @@
+"""
+Getting the Current Date and Time
+We can use a function called datetime.now() to retrieve the current date and time.
+
+from datetime import datetime
+
+print datetime.now()
+The first line imports the datetime library so that we can use it.
+
+The second line will print out the current date and time.
+
+Instructions
+Create a variable called now and store the result of datetime.now() in it.
+Then, print the value of now.
+"""
+from datetime import datetime
+
+now =datetime.now()
+print now
