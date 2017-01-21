d665ad2d464b532ca8d69f3d415c85ec53993adf
haveano <cezary.wysocki+github@gmail.com>
2017-01-21 22:08:43 +0100

    Create 07_upper().py

---
+++ Python Syntax/Strings - Console Output/07_upper().py
@@ -0,0 +1,10 @@
+"""
+upper()
+Now your string is 100% lower case! A similar method exists to make a string completely upper case.
+
+Instructions
+Call upper() on parrot (after print on line 3) in order to capitalize all the characters in the string!
+"""
+parrot = "norwegian blue"
+
+print parrot.upper()
