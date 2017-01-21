0ef8fe7bf785c38f8f6e0607063b9605e7b076e2
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 00:59:41 +0100

    Create 01_List accessing.py

---
+++ 07_Lists and Functions/01_Lists and Functions/01_List accessing.py
@@ -0,0 +1,12 @@
+"""
+List accessing
+This exercise goes over just pulling information from a list, which we've covered in a previous section!
+
+Instructions
+Please add the code to print out the second element in the list.
+"""
+
+n = [1, 3, 5]
+
+# Add your code below
+print n[1]
