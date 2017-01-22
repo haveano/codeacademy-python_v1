5d0c67ea6fc4ceb0cf5b265211589a4c7ea220d6
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:21:24 +0100

    Create 11_For your strings.py

---
+++ 08_Loops/01_Loops/11_For your strings.py
@@ -0,0 +1,21 @@
+"""
+For your strings
+Using a for loop, you can print out each individual character in a string.
+
+The example in the editor is almost plain English: "for each character c in thing, print c".
+
+Instructions
+Add a second for loop so that each character in word is printed one at a time.
+"""
+
+
+thing = "spam!"
+
+for c in thing:
+    print c
+
+word = "eggs!"
+
+# Your code here!
+for x in word:
+    print x
