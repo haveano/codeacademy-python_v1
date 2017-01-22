f661db051e3c52d925a40519fba344b4fbd995b4
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 01:05:46 +0100

    Create 03_Appending to a list.py

---
+++ 07_Lists and Functions/01_Lists and Functions/03_Appending to a list.py
@@ -0,0 +1,13 @@
+"""
+Appending to a list
+Here, we'll quickly recap how to .append() elements to the end of a list.
+
+Instructions
+Append the number 4 to the end of the list n.
+"""
+
+
+n = [1, 3, 5]
+# Append the number 4 here
+n.append(4)
+print n
