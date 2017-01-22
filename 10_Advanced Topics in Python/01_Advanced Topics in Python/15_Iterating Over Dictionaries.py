99ad771f4b78e9edbae769c34f47a96f21a1230e
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 18:04:17 +0100

    Create 15_Iterating Over Dictionaries.py

---
+++ 10_Advanced Topics in Python/01_Advanced Topics in Python/15_Iterating Over Dictionaries.py
@@ -0,0 +1,17 @@
+"""
+Iterating Over Dictionaries
+First, let's review iterating over a dict.
+
+Instructions
+Call the appropriate method on movies such that it will print out all the items (hint, hint) in the dictionary—that is, each key and each value.
+"""
+
+
+movies = {
+	"Monty Python and the Holy Grail": "Great",
+	"Monty Python's Life of Brian": "Good",
+	"Monty Python's Meaning of Life": "Okay"
+}
+print movies.items()
+#for x in movies:
+#    print movies.keys(), movies.values()
