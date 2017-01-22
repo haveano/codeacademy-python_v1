a77676da32eec4efecc16092f93ca66b3a91e29f
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 18:16:17 +0100

    Create 13_Just Flip Out.py

---
+++ 10_Advanced Topics in Python/02_Introduction to Bitwise Operators/13_Just Flip Out.py
@@ -0,0 +1,18 @@
+"""
+Just Flip Out
+Using the XOR (^) operator is very useful for flipping bits. Using ^ on a bit with the number one will return a result where that bit is flipped.
+
+For example, let's say I want to flip all of the bits in a. I might do this:
+
+a = 0b110 # 6
+mask = 0b111 # 7
+desired =  a ^ mask # 0b1
+Instructions
+In the editor is the 8 bit variable a. Use a bitmask and the value a in order to achieve a result where all of the bits in a are flipped. Be sure to print your answer as a bin() string!
+"""
+
+
+a = 0b11101110
+def flip_bits(input):
+    print bin(a^0b11111111)
+flip_bits(a)
