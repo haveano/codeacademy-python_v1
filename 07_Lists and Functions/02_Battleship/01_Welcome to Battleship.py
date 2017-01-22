a1d096997e713e665866d6072a416515cafdae7e
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 16:58:27 +0100

    Create 01_Welcome to Battleship.py

---
+++ 07_Lists and Functions/02_Battleship/01_Welcome to Battleship.py
@@ -0,0 +1,9 @@
+"""
+Welcome to Battleship!
+In this project you will build a simplified, one-player version of the classic board game Battleship! In this version of the game, there will be a single ship hidden in a random location on a 5x5 grid. The player will have 10 guesses to try to sink the ship.
+
+To build this game we will use our knowledge of lists, conditionals and functions in Python. When you're ready to get started, click run to continue.
+
+Instructions
+Click Save & Submit Code to get started with Battleship!
+"""
