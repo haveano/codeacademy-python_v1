eb4b7b1f0b1a1959c5b86e4500bda0f48cb2c96b
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 16:51:06 +0100

    Create 06_More than one argument.py

---
+++ 07_Lists and Functions/01_Lists and Functions/06_More than one argument.py
@@ -0,0 +1,16 @@
+"""
+More than one argument
+This exercise is to recap how to use more than one argument in a function.
+
+Instructions
+Define a function called add_function that has 2 parameters x and y and adds them together.
+"""
+
+m = 5
+n = 13
+# Add add_function here!
+
+
+def add_function(x,y):
+    return x+y
+print add_function(m, n)
