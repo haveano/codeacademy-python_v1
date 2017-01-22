815b5171dd3c64fba6dbac357675819223be0e36
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:25:30 +0100

    Create 19_Create your own.py

---
+++ 08_Loops/01_Loops/19_Create your own.py
@@ -0,0 +1,16 @@
+"""
+Create your own
+To wrap up this lesson, let's create our own for/else statement from scratch.
+
+Instructions
+Build your for/else statement in the editor. Execution of the else branch is optional, but your code should print a string of your choice to the editor regardless.
+"""
+
+x = [1, 2, 3, 5, 6, 7]
+
+for item in x:
+    if item >5:
+        print item
+    else:
+        print "too small!"
+
