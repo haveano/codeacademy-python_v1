c2896f3df9123252e5d1600a8667fdb05a234d15
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:38:04 +0100

    Create 11_count.py

---
+++ 08_Loops/02_Practice Makes Perfect/11_count.py
@@ -0,0 +1,24 @@
+"""
+count
+Great work so far. Let's finish up by practicing with a few functions that take lists as arguments.
+
+Instructions
+Define a function called count that has two arguments called sequence and item.
+Return the number of times the item occurs in the list.
+For example: count([1,2,1,1], 1) should return 3 (because 1 appears 3 times in the list).
+
+There is a list method in Python that you can use for this, but you should do it the long way for practice.
+Your function should return an integer.
+The item you input may be an integer, string, float, or even another list!
+Be careful not to use list as a variable name in your code—it's a reserved word in Python!
+"""
+
+
+def count(sequence,item):
+    found=0
+    for x in sequence:
+        if x==item:
+            found=found+1
+    return found
+    
+print count([1,2,2,3,4,1,3,1,2,1],1)
