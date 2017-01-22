952c6d8c5a037750be77eed3bb51e9d621f375fb
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 16:50:36 +0100

    Create 05_Changing the functionality of a function.py

---
+++ 07_Lists and Functions/01_Lists and Functions/05_Changing the functionality of a function.py
@@ -0,0 +1,14 @@
+"""
+Changing the functionality of a function
+In this exercise, you will just be making a minor change to a function to change what that function does.
+
+Instructions
+Change the function so the given argument is multiplied by 3 and returned.
+"""
+
+number = 5
+
+def my_function(x):
+    return x * 3
+
+print my_function(number)
