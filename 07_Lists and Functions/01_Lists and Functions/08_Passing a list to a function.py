f442d4f80e7155d5e5dd8d385db9ff2f8ec099a2
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 16:52:08 +0100

    Create 08_Passing a list to a function.py

---
+++ 07_Lists and Functions/01_Lists and Functions/08_Passing a list to a function.py
@@ -0,0 +1,13 @@
+"""
+Passing a list to a function
+You pass a list to a function the same way you pass any other argument to a function.
+
+Instructions
+Click Save & Submit Code to see that using a list as an argument in a function is essentially the same as using just a number or string!
+"""
+
+def list_function(x):
+    return x
+
+n = [3, 5, 7]
+print list_function(n)
