309e377aa10b37dcdd5edc9805a0c7e0ff6f2b0d
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:33:30 +0100

    Create 02_is_even.py

---
+++ 08_Loops/02_Practice Makes Perfect/02_is_even.py
@@ -0,0 +1,18 @@
+"""
+is_even
+All right! Let's get started.
+
+Remember how an even number is a number that is divisible by 2?
+
+Instructions
+Define a function is_even that will take a number x as input.
+If x is even, then return True.
+Otherwise, return False.
+"""
+
+def is_even(x):
+    if x%2 == 0:
+        return True
+    else:
+        return False
+        #print x
