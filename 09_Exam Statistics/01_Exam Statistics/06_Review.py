30e866b66797d403f6adcd6b62e12f6c4c4d23da
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:43:44 +0100

    Create 06_Review.py

---
+++ 09_Exam Statistics/01_Exam Statistics/06_Review.py
@@ -0,0 +1,11 @@
+"""
+Review
+Great work creating the capability to compute the average of the test grades.
+
+We're going to use the average for computing the variance. The variance allows us to see how widespread the grades were from the average.
+
+Instructions
+Hit Save & Submit Code to continue.
+"""
+
+print "Time to conquer the variance!"
