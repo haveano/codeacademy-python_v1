9534564173f0320d26f6f17c55d68758c6d477a4
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:20:15 +0100

    Create 09_For your health.py

---
+++ 08_Loops/01_Loops/09_For your health.py
@@ -0,0 +1,14 @@
+"""
+For your health
+An alternative way to loop is the for loop. The syntax is as shown; this example means "for each number i in the range 0 - 9, print i".
+
+Instructions
+Make the loop print the numbers from 0 to 19 instead of 0 to 9.
+"""
+
+print "Counting..."
+
+for i in range(20):
+    print i
+    
+    
