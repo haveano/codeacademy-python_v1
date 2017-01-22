319ebcaee2a47be7e7a2556575a2dcc0890c216d
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 01:00:31 +0100

    Create 02_List element modification.py

---
+++ 07_Lists and Functions/01_Lists and Functions/02_List element modification.py
@@ -0,0 +1,15 @@
+"""
+List element modification
+You've already learned how to modify elements of a list in a previous section. This exercise is just a recap of that!
+
+Instructions
+On line 3, multiply the second element of the n list by 5
+Overwrite the second element with that result.
+Make sure to print the list when you are done!
+"""
+
+
+n = [1, 3, 5]
+# Do your multiplication here
+n[1]=n[1]*5
+print n
