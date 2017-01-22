c765f144d5b11bd863b638231f16159975997258
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:42:25 +0100

    Create 03_Review.py

---
+++ 09_Exam Statistics/01_Exam Statistics/03_Review.py
@@ -0,0 +1,16 @@
+"""
+Review
+So far, you've created a helper function that will be used in the next sections.
+
+You also have a solid handle on the concepts that we'll need to continue.
+
+The next step in the creation of our grade statistics program involves computing the mean (average) of the grades.
+
+Onwards.
+
+Instructions
+Hit Save & Submit Code to continue.
+"""
+
+
+print "Let's compute some stats!"
