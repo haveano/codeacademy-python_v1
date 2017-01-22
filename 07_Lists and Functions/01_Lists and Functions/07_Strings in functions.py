30db1f21480930c5f6bfb19877f0e017b1b6df6b
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 16:51:36 +0100

    Create 07_Strings in functions.py

---
+++ 07_Lists and Functions/01_Lists and Functions/07_Strings in functions.py
@@ -0,0 +1,16 @@
+"""
+Strings in functions
+This is a basic recap on using strings in functions.
+
+Instructions
+Write a function called string_function that takes in a string argument (s) and then returns that argument concatenated with the word 'world'. Don't add a space before world!
+"""
+
+
+n = "Hello"
+# Your function here!
+
+def string_function(s):
+    #s=
+    return s+"world"
+print string_function(n)
