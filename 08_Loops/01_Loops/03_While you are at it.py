3aa2cde4bc353173717c225a6eb147f637f62bfc
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:16:41 +0100

    Create 03_While you are at it.py

---
+++ 08_Loops/01_Loops/03_While you are at it.py
@@ -0,0 +1,17 @@
+"""
+While you're at it
+Inside a while loop, you can do anything you could do elsewhere, including arithmetic operations.
+
+Instructions
+Create a while loop that prints out all the numbers from 1 to 10 squared (1, 4, 9, 16, ... , 100), each on their own line.
+
+Fill in the blank space so that our while loop goes from 1 to 10 inclusive.
+Inside the loop, print the value of num squared. The syntax for squaring a number is num ** 2.
+Increment num.
+"""
+
+num = 1
+
+while num<11:  # Fill in the condition
+    print num*num# Print num squared
+    num=num+1# Increment num (make sure to do this!)
