18709b1f5a651463f61c9713fdc9c617aeb1c20a
haveano <cezary.wysocki+github@gmail.com>
2017-01-22 17:31:52 +0100

    Create 02_Practice Makes Perfect.py

---
+++ 08_Loops/02_Practice Makes Perfect.py
@@ -0,0 +1,11 @@
+"""
+Practice! Practice Practice!
+The best way to get good at anything is a lot of practice. This lesson is full of practice problems for you to work on. This section will contain minimal instructions to help you solve these problems; instead, this section will help you work on taking your programming skills and applying them to real life problems.
+
+The more challenging programs will contain some helpful hints to nudge you in the right direction. If you feel as if you are completely lost, feel free to check out the Q&A section for help (the link is on the very bottom left of your screen).
+
+Instructions
+Hit Save & Submit Code to continue.
+"""
+
+#Nic nie bylo
