0efe1d864004a14820048568872dd26b1c661765
haveano <cezary.wysocki+github@gmail.com>
2017-01-24 20:23:53 +0100

    Create 02_Create an instance of a class.py

---
+++ 11_Introduction to Classes/02_Classes/02_Create an instance of a class.py
@@ -0,0 +1,16 @@
+"""
+Create an instance of a class
+We can use classes to create new objects, which we say are instances of those classes.
+
+Creating a new instance of a class is as easy as saying:
+
+newObject = ClassName()
+Instructions
+Below your Car class, create a new object named my_car that is an instance of Car.
+"""
+
+
+class Car(object):
+    #__init__():
+        pass
+my_car=Car()
