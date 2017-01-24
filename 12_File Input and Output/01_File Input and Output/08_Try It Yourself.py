34d38637d8ac3a14eef61f38e40260a4c4f4e0c1
haveano <cezary.wysocki+github@gmail.com>
2017-01-24 20:36:30 +0100

    Create 08_Try It Yourself.py

---
+++ 12_File Input and Output/01_File Input and Output/08_Try It Yourself.py
@@ -0,0 +1,12 @@
+"""
+Try It Yourself
+It worked! Our Python program successfully wrote to text.txt.
+
+Instructions
+Now you try: write any data you like to the text.txt file using with...as. Give your file object the usual name: my_file.
+"""
+
+
+with open("text.txt", "w") as my_file:
+    my_file.write("jakis ciag znakow do pliku\ni tu dalej")
+
