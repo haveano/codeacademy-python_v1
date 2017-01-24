9337e36a13f14263820467a4d2b95c9330264136
haveano <cezary.wysocki+github@gmail.com>
2017-01-24 20:26:47 +0100

    Create 07_Creating class methods.py

---
+++ 11_Introduction to Classes/02_Classes/07_Creating class methods.py
@@ -0,0 +1,36 @@
+"""
+Creating class methods
+Besides member variables, classes can also have their own methods. For example:
+
+class Square(object):
+  def __init__(self, side):
+    self.side = side
+
+  def perimeter(self):
+    return self.side * 4
+The perimeter() class method is identical to defining any other function, except that it is written inside of the Square class definition.
+
+Just like when we defined __init__(), you need to provide self as the first argument of any class method.
+
+Instructions
+Inside the Car class, add a method named display_car() to Car that will reference the Car's member variables to return the string, "This is a [color] [model] with [mpg] MPG." You can use the str() function to turn your mpg into a string when creating the display string.
+Replace the individual print statements with a single print command that displays the result of calling my_car.display_car()
+"""
+
+
+
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    def display_car(self):
+        print "This is a %s %s with %d MPG." % (self.color, self.model, self.mpg)
+
+my_car = Car("DeLorean", "silver", 88)
+print my_car.condition
+print my_car.model
+print my_car.color
+print my_car.mpg
+print my_car.display_car()
